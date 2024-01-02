c46d1d86f1ea818c5cdc04e8335eeb892985ea93
shyam <tc03071999@gmail.com>
2024-01-02 13:46:15 +0530

    Created a class for HW

---
+++ Car.java
@@ -0,0 +1,15 @@
+package JavaQuestions;
+//● Write a Java program named Car
+//
+//● The Car class should have the following attributes: make (String), model (String) , year (short) , and price(int) .
+//
+//        ● The car class should have a constructor that takes all the attributes.
+//
+//        ● Add a main method to instantiate car objects.
+//
+//        ● The program should allow the user to create and display objects of each Car Class.
+
+public class Car {
+    String Car_make;
+
+}
